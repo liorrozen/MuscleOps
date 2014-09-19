6b29e32acf1f1e3686fe48be1e834a6aa96a33e4
oranb83 <oranb83@github.com>
2014-09-19 10:46:57 +0300

    done

--- boto_api.py
+++ boto_api.py
@@ -18,7 +18,6 @@ class Ec2Api( object ):
             aws_secret_access_key = AWS_SECRET_KEY
         )
 
-        print self.conn
         self.instance_id = None
 
     def create_instance( self, ami = "ami-416b4d28", intance_type = "m1.small" ):
@@ -44,7 +43,7 @@ class SqsApi( object ):
             aws_secret_access_key = AWS_SECRET_KEY
         )
 
-    def create_queue( self, name = "AWSHackathon" ):
+    def create_queue( self, name = "MuscleOpsQ" ):
         return self.conn.create_queue( name )
 
     def list_queues( self ):
@@ -55,16 +54,27 @@ class SqsApi( object ):
         m.set_body( msg )
         q.write( m )
 
+    def read_message( self, q, msg = "If you're sleepy and you know it; clap your hands!" ):
+        rs = q.get_messages()
+        m = rs[ 0 ].get_body() if len( rs ) else msg
+        return m
 
 class S3Api( object ):
 
     def __init__( self ):
         self.conn = S3Connection( AWS_ACCESS_KEY, AWS_SECRET_KEY )
 
-    def create_bucket( self, name ):
+    def create_bucket( self, name = "muscle.ops.bucket" ):
         return self.conn.create_bucket( name )
 
     def upload_string( self, bucket, key = "clap", msg = "If you're sleepy and you know it; clap your hands!" ):
+        try:
+            sqs = SqsApi()
+            sqs.create_queue()
+            msg = sqs.read_message()
+        except Exception as e:
+            pass
+
         k = Key( bucket )
         k.key = key
         k.set_contents_from_string( msg )
@@ -80,9 +90,15 @@ def test_sqs_run():
     sqs = SqsApi()
     sqs.write_message( sqs.create_queue() )
 
+def test_s3_create_bucket():
+    s3 = S3Api()
+    s3.upload_string( s3.create_bucket() )
+
+
 if __name__ == "__main__":
     #ec2 = Ec2Api()
     #test_ec2_create( ec2 )
     #test_ec2_stop( ec2 )
-    test_sqs_run()
+    #test_sqs_run()
 
+    test_s3_create_bucket()
